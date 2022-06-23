6abdc06e77c346627628d53f50a854b1b20d75df
Eduardo <barrosoeduardo64@gmail.com>
2022-06-23 01:08:58 -0300

    adição de custumização da feature video

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,41 +1,101 @@
-import { DiscordLogo, Lightning } from "phosphor-react";
+import { DblClickFullscreen, DefaultUi, Player, Youtube } from "@vime/react";
+import { CaretRight, DiscordLogo, FileArrowDown, Image, Lightning } from "phosphor-react";
+import '@vime/core/themes/default.css'
+
 
 export function Video() {
     return (
         <div className="flex-1">
 
-            <iframe
-                className="w-[100%] h-[20rem]"
-                src="  https://youtu.be/iZq0u3quAqo?list=RDiZq0u3quAqo">
-            </iframe>
-            <div className="flex flex-1 items-start justify-between mx-8 mt-8 mb-6  gap-[60px]">
-                <div className="text-base">
-                    <h2 className="text-gray-100  font-bold mb-4">
-                        Aula 01 - Criando o projeto e realizando o setup inicial
-                    </h2>
-                    <p className="text-gray-300 font-normal">
-                        Nessa aula vamos dar início ao projeto criando a estrutura 
-                        base da aplicação utilizando ReactJS,
-                         Vite e TailwindCSS. Vamos também realizar o setup do nosso
-                          projeto no GraphCMS criando as entidades 
-                         da aplicação e integrando a API GraphQL gerada pela plataforma no 
-                         nosso front-end utilizando Apollo Client.
-                    </p>
+            <div className="bg-black flex justify-center items-center">
+                <div className="w-full h-full max-w-[1100px] max-h-[60vh] aspect-video bg-white">
+                    <Player>
+                        <Youtube videoId="KJj70dBgRPo"/>
+                        <DefaultUi />
+                        <DblClickFullscreen />
+                    </Player>
                 </div>
-                <div>
-                    <button className="w-[14.813rem]  rounded bg-green-500 font-bold text-sm text-gray-100 flex items-center justify-center gap-1 py-4 mb-4">
-                        <DiscordLogo size={24}/>
-                        <span>
-                            COMUNIDADE NO DISCORDE
-                        </span>
-                    </button>
-                    <button className="w-[14.813rem] rounded border  border-blue-500 font-bold text-sm text-blue-500 flex items-center justify-center gap-1 py-4 ">
-                        <Lightning size={24}/>
-                        <span>
-                            ACESSE O DESAFIO
-                        </span>
-                    </button>
-                    
+            </div>
+            <div className=" max-w-[1100px] mx-auto  p-8 ">
+                <div className="flex items-start gap-16">
+                    <div className="flex-1">
+                        <h1 className="text-gray-100 text-2xl font-bold mb-4">
+                            Aula 01 - Criando o projeto e realizando o setup inicial
+                        </h1>
+                        <p className="text-gray-200 font-normal leading-relaxed">
+                            Nessa aula vamos dar início ao projeto criando a estrutura 
+                            base da aplicação utilizando ReactJS,
+                            Vite e TailwindCSS. Vamos também realizar o setup do nosso
+                            projeto no GraphCMS criando as entidades 
+                            da aplicação e integrando a API GraphQL gerada pela plataforma no 
+                            nosso front-end utilizando Apollo Client.
+                        </p>
+                        <div className="flex items-center gap-4 mt-6">
+                            <img
+                                className="rounded-full h-16 border-2 border-blue-500" 
+                                src="https://github.com/Eduardo-BarrosoS.png" 
+                                alt="Foto de perfil" />
+                            <div className="leading-relaxed"> 
+                                <strong className="font-bold text-2xl block">
+                                    Eduardo Barroso
+                                </strong>
+                                <span className="text-gray-200 text-sm block">
+                                    Educador
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="flex flex-col gap-4">
+                        <a href="#" className=" uppercase rounded bg-green-500 font-bold text-sm flex items-center justify-center gap-1 p-4 hover:bg-green-700 transition-colors">
+                            <DiscordLogo size={24}/>
+                            <span>
+                                Comunidade do discord
+                            </span>
+                        </a>
+                        <button className="rounded border  border-blue-500 font-bold text-sm text-blue-500 flex items-center justify-center gap-1 p-4 hover:bg-blue-500 hover:text-gray-900 transition-colors">
+                            <Lightning size={24}/>
+                            <span>
+                                ACESSE O DESAFIO
+                            </span>
+                        </button>
+                    </div>
+
+                </div>
+                <div className="gap-8 mt-20 grid grid-cols-2">
+                    <a href="" className="bg-gray-700 rounded overflow-hidden flex items-stretch gap-6 hover: bg-gray-600 transition-colors">
+                        <div className="bg-green-700 h-full p-6 flex items-center">
+                            <FileArrowDown size={24}/> 
+                        </div>
+                        <div className="py-6 leading-relaxed">
+                            <strong className="text-2xl">
+                                Material complementar
+                            </strong>
+                            <p className="text-sm text-gray-200 mt-2">
+                            Acesse o material complementar para acelerar o seu desenvolvimento
+                            </p>
+                        </div>
+                        <div className=" h-full p-6 flex items-center text-blue-500">
+                            <CaretRight size={24}/>
+                        </div>
+                    </a>
+                    <a href="" className="bg-gray-700 rounded overflow-hidden flex items-stretch gap-6 hover: bg-gray-600 transition-colors">
+                        <div className="bg-green-700 h-full p-6 flex items-center">
+                            <Image size={24}/> 
+                        </div>
+                        <div className="py-6 leading-relaxed">
+                            <strong className="text-2xl">
+                                Wallpapers exclusivos
+                            </strong>
+                            <p className="text-sm text-gray-200 mt-2">
+                                Baixe wallpapers exclusivos do Ignite Lab e personalize a sua máquina
+                            </p>
+                        </div>
+                        <div className=" h-full p-6 flex items-center text-blue-500">
+                            <CaretRight size={24}/>
+                        </div>
+                    </a>
+
                 </div>
             </div>
 
